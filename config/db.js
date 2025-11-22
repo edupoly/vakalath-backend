0acbc0ea0a8847ee1169326a14cadfc66f2e639f
SriHaran23 <sriharankandepalli@gmail.com>
2025-11-22 10:50:16 +0530

    mongo_url static

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://sriharan_23:66sY6G9aApV-T85@cluster0.s3gz2cg.mongodb.net/vakalath?retryWrites=true&w=majority&appName=Cluster0");
+    // await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connectedd");
   } catch (err) {
     console.error(err.message);
